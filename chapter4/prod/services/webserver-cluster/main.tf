bbdcac5eed399fafd7f7b303727fc9d1db95d83f
ballan <ballan00@gmail.com>
2020-06-16 22:43:51 -0500

    working on chapter 4 modules

---
+++ chapter4/prod/services/webserver-cluster/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-2"
+}
+
+module "webserver_cluster" {
+    source = "../../../modules/services/webserver-cluster"
+
+    cluster_name = "webservers-prod"
+    db_remote_state_bucket = "ballan-terraform-state"
+    db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+}
+
