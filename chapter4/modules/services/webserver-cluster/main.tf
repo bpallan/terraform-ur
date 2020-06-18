b35d70b0179e21dcd3fdfee358ff2a52ba5546aa
ballan <ballan00@gmail.com>
2020-06-17 22:37:58 -0500

    working on chapter 4 modules

--- chapter4/modules/services/webserver-cluster/main.tf
+++ chapter4/modules/services/webserver-cluster/main.tf
@@ -1,3 +1,11 @@
+locals {
+  http_port = 80
+  any_port = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips = ["0.0.0.0/0"]
+}
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -17,7 +25,7 @@ data "terraform_remote_state" "db" {
 }
 
 data "template_file" "user_data" {
-  template = file("user-data.sh")
+  template = file("${path.module}/user-data.sh")
 
   vars = {
     server_port = var.server_port
@@ -28,7 +36,7 @@ data "template_file" "user_data" {
 
 resource "aws_launch_configuration" "example" {
   image_id               = "ami-0edf3b95e26a682df"
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   security_groups        = [aws_security_group.instance.id]
 
   user_data = data.template_file.user_data.rendered
@@ -47,8 +55,8 @@ resource "aws_autoscaling_group" "example" {
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
 
-  min_size  = 2
-  max_size  = 10
+  min_size  = var.min_size
+  max_size  = var.max_size
 
   tag {
     key                             = "Name"
@@ -58,15 +66,17 @@ resource "aws_autoscaling_group" "example" {
 }
 
 resource "aws_security_group" "instance" {
-
   name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_server_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+
+  from_port = var.server_port
+  to_port = var.server_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb" "example" {
@@ -78,7 +88,7 @@ resource "aws_lb" "example" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.example.arn
-  port              = 80
+  port              = local.http_port
   protocol          = "HTTP"
 
   # By default, return a simple 404 page
@@ -94,23 +104,27 @@ resource "aws_lb_listener" "http" {
 }
 
 resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
+  name = "${var.cluster_name}-alb"  
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
 
-  # Allow all outbound requests
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
+
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
