6496ea708ae28acb567186943a5bcf5dc08f2dc5
ballan <ballan00@gmail.com>
2020-06-25 22:44:09 -0500

    fixed chapter 4 stage

--- chapter4/modules/services/webserver-cluster/variables.tf
+++ chapter4/modules/services/webserver-cluster/variables.tf
@@ -1,35 +1,45 @@
-variable "server_port" {
-  description   = "The port the service will use for HTTP requests"
-  type          = number
-  default       = 8080
-}
+# ---------------------------------------------------------------------------------------------------------------------
+# REQUIRED PARAMETERS
+# You must provide a value for each of these parameters.
+# ---------------------------------------------------------------------------------------------------------------------
 
 variable "cluster_name" {
   description = "The name to use for all the cluster resources"
-  type = string
+  type        = string
 }
 
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket for the database's remote state"
-  type = string
+  type        = string
 }
 
 variable "db_remote_state_key" {
   description = "The path for the database's remote state in S3"
-  type = string
+  type        = string
 }
 
 variable "instance_type" {
-  description = "The type of EC2 instances to run (e.g. t2.micro)"
-  type = string
+  description = "The type of EC2 Instances to run (e.g. t2.micro)"
+  type        = string
 }
 
 variable "min_size" {
-  description = "The minimum number of EC2 instances in the ASG"
-  type = number
+  description = "The minimum number of EC2 Instances in the ASG"
+  type        = number
 }
 
 variable "max_size" {
-  description = "The maximum number of EC2 instances in the ASG"
-  type = number
+  description = "The maximum number of EC2 Instances in the ASG"
+  type        = number
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# These parameters have reasonable defaults.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
 }
