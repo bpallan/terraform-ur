fabe009e7724e7d5f2b32c2f8cfe414d353951ed
ballan <ballan00@gmail.com>
2020-07-13 22:37:00 -0500

    chapter 6 wip

---
+++ chapter6/global/iam/outputs.tf
@@ -0,0 +1,18 @@
+# using count
+#output "neo_arn" {
+#    value = aws_iam_user.example[0].arn
+#    description = "The ARN for user Neo"
+#}
+
+#output "all_arns" {
+#    value = aws_iam_user.example[*].arn
+#    description = "The ARNs for all users"
+#}
+
+output "all_users" {
+    value = aws_iam_user.example
+}
+
+output "all_arns" {
+    value = values(aws_iam_user.example)[*].arn
+}
